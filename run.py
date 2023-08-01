c26212c1aeb665a01c8d8eb7511174b22dd6e44e
Lazarus <syw09191319@gmail.com>
2023-08-01 17:05:26 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import git
 import time
+from datetime import datetime
 
 def repos_data_update():
     global repos
-    
+
     with open('repos.txt', 'r') as f:
         repos = []
         for line in map(lambda line: line.strip(), f.readlines()):
@@ -19,16 +20,16 @@ while len(repos) > 0:
         repo = git.Repo(repo)
         name = repo.working_tree_dir.split("\\")[-1].split("/")[-1]
 
-        print(f'[ GIT UPDATE CHECK: {name} ] Checking for Github update...')
+        print(f'[ {datetime.now():%Y-%m-%d %H:%M:%S} GIT UPDATE CHECK: {name} ] Checking for Github update...')
 
         if repo.bare:
-            print(f'[ GIT UPDATE CHECK: {name} ] ERROR: Bare repository is not supported.\n')
+            print(f'[ {datetime.now():%Y-%m-%d %H:%M:%S} GIT UPDATE CHECK: {name} ] ERROR: Bare repository is not supported.\n')
             continue
         
         try:
             remote = repo.remote()
         except:
-            print(f'[ GIT UPDATE CHECK: {name} ] ERROR: Remote not found\n')
+            print(f'[ {datetime.now():%Y-%m-%d %H:%M:%S} GIT UPDATE CHECK: {name} ] ERROR: Remote not found\n')
             continue
 
         remote.fetch()
@@ -37,14 +38,14 @@ while len(repos) > 0:
         after = list(repo.iter_commits('FETCH_HEAD'))
 
         if before[0].hexsha != after[0].hexsha:
-            print(f'[ GIT UPDATE CHECK: {name} ] Update found in Github!')
+            print(f'[ {datetime.now():%Y-%m-%d %H:%M:%S} GIT UPDATE CHECK: {name} ] Update found in Github!')
 
             remote.pull()
             remote.update()
 
-            print(f'[ GIT UPDATE CHECK: {name} ] Repository successfully updated!\n')
+            print(f'[ {datetime.now():%Y-%m-%d %H:%M:%S} GIT UPDATE CHECK: {name} ] Repository successfully updated!\n')
         else:
-            print(f'[ GIT UPDATE CHECK: {name} ] Update not found\n')
+            print(f'[ {datetime.now():%Y-%m-%d %H:%M:%S} GIT UPDATE CHECK: {name} ] Update not found\n')
     
     repos_data_update()
 
