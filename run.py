92c777946135ffaf4469d17331f72d7e497e15c3
Lazarus <syw09191319@gmail.com>
2023-09-04 09:28:19 +0900

    Update run.py

--- run.py
+++ run.py
@@ -50,7 +50,7 @@ def main():
             try:
                 repo = git.Repo(repo_data['path'])
             except:
-                print(sys.exc_info[1])
+                print(sys.exc_info()[1])
                 continue
 
             name = repo.working_tree_dir.split("\\")[-1].split("/")[-1]
@@ -140,4 +140,4 @@ while check_internet():
     except KeyboardInterrupt:
         exit()
     except:
-        print(sys.exc_info[1])
+        print(sys.exc_info()[1])
